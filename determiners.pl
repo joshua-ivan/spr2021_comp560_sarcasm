a4d133f93c11d1b109d93ad1aa807cceb3f598d0
KARIM ATALLA <62119150+katalla1@users.noreply.github.com>
2021-04-23 16:59:44 -0700

    Add files via upload

--- determiners.pl
+++ determiners.pl
@@ -1,146 +1,268 @@
-det-> ["a"]
-det-> ["a","few"]
-det-> ["a","little"]
-det-> ["all"]
-det-> ["another"]
-det-> ["any"]
-det-> ["anybody"]
-det-> ["anyone"]
-det-> ["anything"]
-det-> ["anywhere"]
-det-> ["both"]
-det-> ["certain"]
-det-> ["each"]
-det-> ["either"]
-det-> ["enough"]
-det-> ["every"]
-det-> ["everybody"]
-det-> ["everyone"]
-det-> ["everything"]
-det-> ["everywhere"]
-det-> ["few"]
-det-> ["fewer"]
-det-> ["fewest"]
-det-> ["last"]
-det-> ["least"]
-det-> ["less"]
-det-> ["little"]
-det-> ["many"]
-det-> ["many","a"]
-det-> ["more"]
-det-> ["most"]
-det-> ["much"]
-det-> ["neither"]
-det-> ["next"]
-det-> ["no"]
-det-> ["no","one"]
-det-> ["nobody"]
-det-> ["none"]
-det-> ["nothing"]
-det-> ["nowhere"]
-det-> ["once"]
-det-> ["one"]
-det-> ["said"]
-det-> ["several"]
-det-> ["some"]
-det-> ["somebody"]
-det-> ["something"]
-det-> ["somewhere"]
-det-> ["sufficient"]
-det-> ["that"]
-det-> ["the"]
-det-> ["these"]
-det-> ["this"]
-det-> ["those"]
-det-> ["three"]
-det-> ["thrice"]
-det-> ["twice"]
-det-> ["two"]
-det-> ["us"]
-det-> ["various"]
-det-> ["we"]
-det-> ["what"]
-det-> ["whatever"]
-det-> ["which"]
-det-> ["whichever"]
-det-> ["you"]
-det-> ["zero"]
+det--> ["a"].
+det--> ["a","few"].
+det--> ["a","little"].
+det--> ["all"].
+det--> ["another"].
+det--> ["any"].
+det--> ["anybody"].
+det--> ["anyone"].
+det--> ["anything"].
+det--> ["anywhere"].
+det--> ["both"].
+det--> ["certain"].
+det--> ["each"].
+det--> ["either"].
+det--> ["enough"].
+det--> ["every"].
+det--> ["everybody"].
+det--> ["everyone"].
+det--> ["everything"].
+det--> ["everywhere"].
+det--> ["few"].
+det--> ["fewer"].
+det--> ["fewest"].
+det--> ["last"].
+det--> ["least"].
+det--> ["less"].
+det--> ["little"].
+det--> ["many"].
+det--> ["many","a"].
+det--> ["more"].
+det--> ["most"].
+det--> ["much"].
+det--> ["neither"].
+det--> ["next"].
+det--> ["no"].
+det--> ["no","one"].
+det--> ["nobody"].
+det--> ["none"].
+det--> ["nothing"].
+det--> ["nowhere"].
+det--> ["once"].
+det--> ["one"].
+det--> ["said"].
+det--> ["several"].
+det--> ["some"].
+det--> ["somebody"].
+det--> ["something"].
+det--> ["somewhere"].
+det--> ["sufficient"].
+det--> ["that"].
+det--> ["the"].
+det--> ["these"].
+det--> ["this"].
+det--> ["those"].
+det--> ["three"].
+det--> ["thrice"].
+det--> ["twice"].
+det--> ["two"].
+det--> ["us"].
+det--> ["various"].
+det--> ["we"].
+det--> ["what"].
+det--> ["whatever"].
+det--> ["which"].
+det--> ["whichever"].
+det--> ["you"].
+det--> ["zero"].
 
-conj->["For"]
-conj->["And"]
-conj->["Nor"]
-conj->["But"]
-conj->["Or"]
-conj->["Yet"]
-conj->["So"]
-conj->["although"]
-conj->["after"]
-conj->["as"]
-conj->["while"]
-conj->["when"]
-conj->["whereas"]
-conj->["whenever"]
-conj->["wherever"]
-conj->["whether"]
-conj->["how"]
-conj->["if"]
-conj->["in","case"]
-conj->["in","order","that"]
-cinj->["in","order","to"]
-conj->["though"]
-conj->["even","though"]
-conj->["even","if"]
-conj->["because"]
-conj->["before"]
-conj->["until"]
-conj->["unless"]
-conj->["since"]
-conj->["so"]
-conj->["so","that"]
-conj->["Not","only"]
-conj->["but","also"]
-conj->["As"]
-conj->["as"]
-conj->["Both"]
-conj->["and"]
-conj->["Either"]
-conj->["or"]
-conj->["Whether"]
-conj->["Neither"]
-conj->["nor"]
-conj->["Not"]
-conj->["but"]
-conj->["Such"]
-conj->["that"]
-conj->["Scarcely"]
-conj->["when"]
-conj->["As","many"]
-conj->["Rather"]
-conj->["than"]
-conj->["No","sooner"]
-conj->["Accordingly"]
-conj->["After"]
-conj->["Also"]
-conj->["Before"]
-conj->["Besides"]
-conj->["Consequently"]
-conj->["Conversely"]
-conj->["Finally"]
-conj->["Furthermore"]
-conj->["Hence"]
-conj->["However"]
-conj->["Indeed"]
-conj->["Instead"]
-conj->["Likewise"]
-conj->["Meanwhile"]
-conj->["Moreover"]
-conj->["Nevertheless"]
-conj->["Next"]
-conj->["Nonetheless"]
-conj->["Otherwise"]
-conj->["Similarly"]
-conj->["Still"]
-conj->["Subsequently"]
-conj->["Then"]
-conj->["Therefore"]
-conj->["Thus"]
+conj-->["For"].
+conj-->["And"].
+conj-->["Nor"].
+conj-->["But"].
+conj-->["Or"].
+conj-->["Yet"].
+conj-->["So"].
+conj-->["although"].
+conj-->["after"].
+conj-->["as"].
+conj-->["while"].
+conj-->["when"].
+conj-->["whereas"].
+conj-->["whenever"].
+conj-->["wherever"].
+conj-->["whether"].
+conj-->["how"].
+conj-->["if"].
+conj-->["in","case"].
+conj-->["in","order","that"].
+cinj-->["in","order","to"].
+conj-->["though"].
+conj-->["even","though"].
+conj-->["even","if"].
+conj-->["because"].
+conj-->["before"].
+conj-->["until"].
+conj-->["unless"].
+conj-->["since"].
+conj-->["so"].
+conj-->["so","that"].
+conj-->["Not","only"].
+conj-->["but","also"].
+conj-->["As"].
+conj-->["as"].
+conj-->["Both"].
+conj-->["and"].
+conj-->["Either"].
+conj-->["or"].
+conj-->["Whether"].
+conj-->["Neither"].
+conj-->["nor"].
+conj-->["Not"].
+conj-->["but"].
+conj-->["Such"].
+conj-->["that"].
+conj-->["Scarcely"].
+conj-->["when"].
+conj-->["As","many"].
+conj-->["Rather"].
+conj-->["than"].
+conj-->["No","sooner"].
+conj-->["Accordingly"].
+conj-->["After"].
+conj-->["Also"].
+conj-->["Before"].
+conj-->["Besides"].
+conj-->["Consequently"].
+conj-->["Conversely"].
+conj-->["Finally"].
+conj-->["Furthermore"].
+conj-->["Hence"].
+conj-->["However"].
+conj-->["Indeed"].
+conj-->["Instead"].
+conj-->["Likewise"].
+conj-->["Meanwhile"].
+conj-->["Moreover"].
+conj-->["Nevertheless"].
+conj-->["Next"].
+conj-->["Nonetheless"].
+conj-->["Otherwise"].
+conj-->["Similarly"].
+conj-->["Still"].
+conj-->["Subsequently"].
+conj-->["Then"].
+conj-->["Therefore"].
+conj-->["Thus"].
+
+pronoun-->["I"].
+pronoun-->["all"].
+pronoun-->["another"].
+pronoun-->["any"].
+pronoun-->["anybody"].
+pronoun-->["anyone"].
+pronoun-->["anything"].
+pronoun-->["as"].
+pronoun-->["aught"].
+pronoun-->["both"].
+pronoun-->["each"].
+pronoun-->["each","other"].
+pronoun-->["either"].
+pronoun-->["enough"].
+pronoun-->["everybody"].
+pronoun-->["everyone"].
+pronoun-->["everything"].
+pronoun-->["few"].
+pronoun-->["he"].
+pronoun-->["her"].
+pronoun-->["hers"].
+pronoun-->["herself"].
+pronoun-->["him"].
+pronoun-->["himself"].
+pronoun-->["his"].
+pronoun-->["idem"].
+pronoun-->["it"].
+pronoun-->["its"].
+pronoun-->["itself"].
+pronoun-->["many"].
+pronoun-->["me"].
+pronoun-->["mine"].
+pronoun-->["most"].
+pronoun-->["my"].
+pronoun-->["myself"].
+pronoun-->["naught"].
+pronoun-->["neither"].
+pronoun-->["no","one"].
+pronoun-->["nobody"].
+pronoun-->["none"].
+pronoun-->["nothing"].
+pronoun-->["nought"].
+pronoun-->["one"].
+pronoun-->["one","another"].
+pronoun-->["other"].
+pronoun-->["others"].
+pronoun-->["ought"].
+pronoun-->["our"].
+pronoun-->["ours"].
+pronoun-->["ourself"].
+pronoun-->["ourselves"].
+pronoun-->["several"].
+pronoun-->["she"].
+pronoun-->["some"].
+pronoun-->["somebody"].
+pronoun-->["someone"].
+pronoun-->["something"].
+pronoun-->["somewhat"].
+pronoun-->["such"].
+pronoun-->["suchlike"].
+pronoun-->["that"].
+pronoun-->["thee"].
+pronoun-->["their"].
+pronoun-->["theirs"].
+pronoun-->["theirself"].
+pronoun-->["theirselves"].
+pronoun-->["them"].
+pronoun-->["themself"].
+pronoun-->["themselves"].
+pronoun-->["there"].
+pronoun-->["these"].
+pronoun-->["they"].
+pronoun-->["thine"].
+pronoun-->["this"].
+pronoun-->["those"].
+pronoun-->["thou"].
+pronoun-->["thy"].
+pronoun-->["thyself"].
+pronoun-->["us"].
+pronoun-->["we"].
+pronoun-->["what"].
+pronoun-->["whatever"].
+pronoun-->["whatnot"].
+pronoun-->["whatsoever"].
+pronoun-->["whence"].
+pronoun-->["where"].
+pronoun-->["whereby"].
+pronoun-->["wherefrom"].
+pronoun-->["wherein"].
+pronoun-->["whereinto"].
+pronoun-->["whereof"].
+pronoun-->["whereon"].
+pronoun-->["wherever"].
+pronoun-->["wheresoever"].
+pronoun-->["whereto"].
+pronoun-->["whereunto"].
+pronoun-->["wherewith"].
+pronoun-->["wherewithal"].
+pronoun-->["whether"].
+pronoun-->["which"].
+pronoun-->["whichever"].
+pronoun-->["whichsoever"].
+pronoun-->["who"].
+pronoun-->["whoever"].
+pronoun-->["whom"].
+pronoun-->["whomever"].
+pronoun-->["whomso"].
+pronoun-->["whomsoever"].
+pronoun-->["whose"].
+pronoun-->["whosever"].
+pronoun-->["whosesoever"].
+pronoun-->["whoso"].
+pronoun-->["whosoever"].
+pronoun-->["ye"].
+pronoun-->["yon"].
+pronoun-->["yonder"].
+pronoun-->["you"].
+pronoun-->["your"].
+pronoun-->["yours"].
+pronoun-->["yourself"].
+pronoun-->["yourselves "].
