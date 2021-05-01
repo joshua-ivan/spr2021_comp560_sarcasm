46d9b00c25b93403a6e09ed3d0ea6e74f2d718b5
JoshL <joshua.licudo.753@my.csun.edu>
2021-04-30 18:25:18 -0700

    Allow processing random samples of cleaned tweets

--- config.pl
+++ config.pl
@@ -5,3 +5,4 @@ config(normal_data_file_name, "data/normalTweets.pl") :- !.
 config(sarcasm_score_lower_bound, 0.175) :- !.
 config(sarcasm_score_upper_bound, 0.225) :- !.
 config(results_file_name, "results.csv") :- !.
+config(random_sample_size, 250) :- !.
--- driver.pl
+++ driver.pl
@@ -4,15 +4,33 @@
 :- use_module('sentiment_scoring').
 :- ensure_loaded('config').
 
-append_csv(Csv, Line, Pos, Neg, Sarcastic) :-
+append_csv(Csv, Text, Pos, Neg, Sarcastic) :-
     open(Csv, append, Results),
-    format(Results, '\"~s\",~f,~f,~a\n', [Line, Pos, Neg, Sarcastic]),
+    format(Results, '\"~s\",~f,~f,~a\n', [Text, Pos, Neg, Sarcastic]),
     close(Results).
 
-process_(_, end_of_file) :- !.
-process_(Stream, _) :-
-    config(results_file_name, ResultsCsv),
+process_tweets_(_, Count, _) :-
+    config(random_sample_size, Count), !.
+process_tweets_(Database, Count, ResultsCsv) :-
+    database_size(DB_size),
+    TweetID is random(DB_size),
+    tweet(TweetID, Tweet),
 
+    split_string(Tweet, " ", " ", Phrases),
+    score(Phrases, Pos, Neg),
+    is_sarcasm(Pos, Neg, Sarcastic),
+    append_csv(ResultsCsv, Tweet, Pos, Neg, Sarcastic),
+
+    NewCount is Count + 1,
+    process_tweets_(Database, NewCount, ResultsCsv).
+
+process_tweets(Database) :-
+    use_module(Database),
+    config(results_file_name, ResultsCsv),
+    process_tweets_(Database, 0, ResultsCsv).
+
+process_text_(_, end_of_file, _) :- !.
+process_text_(Stream, _, ResultsCsv) :-
     read_line_to_string(Stream, Line),
     string_length(Line, Length),
     ( Length > 0 -> (
@@ -21,8 +39,9 @@ process_(Stream, _) :-
         is_sarcasm(Pos, Neg, Sarcastic),
         append_csv(ResultsCsv, Line, Pos, Neg, Sarcastic)
     ); true ),
-    process_(Stream, Line).
+    process_text_(Stream, Line, ResultsCsv).
 
 process_text(File) :-
     open(File, read, Stream),
-    process_(Stream, "").
+    config(results_file_name, ResultsCsv),
+    process_text_(Stream, "", ResultsCsv).
