c05ac2c625766cb0e3b7e33b0c777e8f58ea472f
KARIM ATALLA <62119150+katalla1@users.noreply.github.com>
2021-04-30 17:07:37 -0700

    Add files via upload

--- sarcasm.pl
+++ sarcasm.pl
@@ -1,25 +1,11 @@
-:- consult([sentiword,determiners,sarcasmRules]).
+:- consult([sarcasmRules]).
 
-n(X,positive):-noun(X,A,B),A>0, A>B.
-n(X,negative):-noun(X,A,B),B>A,B>0.
-n(X,neutral):-noun(X,A,B),B=A,A=0.
 
-v(X,positive):-verb(X,A,B),A>0, A>B.
-v(X,negative):-verb(X,A,B),B>A,B>0.
-v(X,neutral):-verb(X,A,B),B=A,A=0.
-
-adj(X,positive):-adjective(X,A,B),A>0, A>B.
-adj(X,negative):-adjective(X,A,B),B>A,B>0.
-adj(X,neutral):-adjective(X,A,B),B=A.
-
-adv(X,positive):-adverb(X,A,B),A>0, A>B.
-adv(X,negative):-adverb(X,A,B),B>A,B>0.
-adve(X,neutral):-adverb(X,A,B),B=A.
 
 start:- enter_sentence.
 enter_sentence:-
 write('Enter a sentence to compute: '), read(Atom),atom_string(Atom,String),write('Is sentence sarcastic? '),
-      split_string(String, "\s", "\s", [X]),sarcasm([X]).
+      split_string(String, "\s", "\s", X),  sarcastic_sentence(X,[]).
 
 
 
--- sarcasmRules.pl
+++ sarcasmRules.pl
@@ -1,47 +1,114 @@
+:- consult([sentiword,determiners]).
 
-sarcasm-->phrases(negative), phrases(positive).
-sarcasm-->phrases(positive), phrases(negative).
-sarcasm-->phrases(neutral), phrases(positive).
+n(X,positive):-noun(X,A,B),A>0, A>B.
+n(X,negative):-noun(X,A,B),B>A,B>0.
+n(X,neutral):-noun(X,A,B),B=A,A=0.
 
-phrases(positive)--> np(positive), vp(positive).
-phrases(neutral)--> np(neutral), vp(neutral).
-phrases(negative)--> np(negative), vp(negative).
+v(X,positive):-verb(X,A,B),A>0, A>B.
+v(X,negative):-verb(X,A,B),B>A,B>0.
+v(X,neutral):-verb(X,A,B),B=A,A=0.
 
-phrases(positive)-->vp(positive).
-phrases(negative)--> np(negative).
+adj(X,positive):-adjective(X,A,B),A>0, A>B.
+adj(X,negative):-adjective(X,A,B),B>A,B>0.
+adj(X,neutral):-adjective(X,A,B),B=A.
 
-phrases(negative)--> adj(negative), np(_).
-phrases(negative)--> adj(negative), np(_) ,vp(negative).
-phrases(negative)-->vp(negative).
+adv(X,positive):-adverb(X,A,B),A>0, A>B.
+adv(X,negative):-adverb(X,A,B),B>A,B>0.
+adv(X,neutral):-adverb(X,A,B),B=A.
 
+sarcastic_sentence--> sarcasm.
 
-np(positive)-->n(_,positive).
-np(positive)-->adj(_,positive), n(_,positive).
-np(positive)-->adj(_,positive), n(_, nuetral).
+sarcasm--> negphrases, posphrases.
+sarcasm--> posphrases, negphrases.
+sarcasm--> neuphrases, posphrases.
 
+sarcasm--> negphrases, conj, posphrases.
+sarcasm--> posphrases, conj, negphrases.
+sarcasm--> negphrases, conj, posphrases.
 
-np(neutral)--> n(_,neutral).
-np(neutral)--> adj(_,neutral), n(_,neutral).
-
-
-np(negative)--> n(_,negative).
-np(negative)--> adj(_,negative), n(_,neutral).
-np(negative)--> adj(_,negative), n(_,negative).
-
-
-vp(positive)--> pronoun, v(_,postive).
-vp(positive)--> adj(_,positive), v(_,neutral).
-vp(positive)--> adj(_,positive), v(_,positive).
-vp(positive)--> v(_,positive), adj(_,neutral).
-vp(positive)--> v(_,positive), adj(_,positive).
+sarcasm--> conj, negphrases, posphrases.
+sarcasm--> conj, posphrases, negphrases.
+sarcasm--> conj, neuphrases, posphrases.
 
 
-vp(neutral)--> v(_,neutral).
-vp(neutral)--> adj(_,neutral), v(_,neutral).
+posphrases--> posNP, posVP.
+posphrases-->posVP.
+
+neuphrases--> neuNP, neuVP.
+
+negphrases--> negNP, negVP.
+negphrases--> negNP.
+negphrases--> adj(_,negative), negNP.
+negphrases--> adj(_,negative), posNP.
+negphrases--> adj(_,negative), neuNP.
+negphrases--> adj(_,negative), negNP, negVP.
+negphrases--> adj(_,negative), neuNP, negVP.
+negphrases--> adj(_,negative), posNP, negVP.
+negphrases--> negVP.
 
 
-vp(negative)--> v(_,negative).
-vp(negative)--> adj(_,negative), v(_,neutral).
-vp(negative)--> adj(_,negative), v(_,negative).
-vp(negative)--> v(_,neutral), adj(_,negative).
-vp(negative)--> v(_,negative), adj(_,negative).
+/************************************noun phrases***********************************/
+
+posNP-->n(_,positive).
+posNP-->det, n(_,positive).
+posNP-->adj(_,positive), n(_,positive).
+posNP-->adj(_,positive), n(_, nuetral).
+posNP-->det, adj(_,positive), n(_,positive).
+posNP-->det, adj(_,positive), n(_, nuetral).
+
+
+neuNP--> pronoun.
+neuNP--> n(_,neutral).
+neuNP--> det, n(_,neutral).
+neuNP--> adj(_,neutral), n(_,neutral).
+neuNP--> det, adj(_,neutral), n(_,neutral).
+
+negNP--> n(_,negative).
+negNP--> det, n(_,negative).
+negNP--> det, adj(_,negative), n(_,neutral).
+negNP--> adj(_,negative), n(_,neutral).
+negNP--> det, adj(_,negative), n(_,negative).
+negNP--> adj(_,negative), n(_,negative).
+
+/***********************************verb phrases**************************************/
+
+posVP-->  v(_,positive).
+posVP--> pronoun, v(_,positive).
+posVP--> adj(_,positive), v(_,neutral).
+posVP--> adj(_,positive), v(_,positive).
+posVP--> v(_,positive), adj(_,neutral).
+posVP--> v(_,positive), adj(_,positive).
+posVP--> adv(_,positive), v(_,positive).
+posVP--> adv(_,neutral), v(_,positive).
+posVP--> adv(_,positive), v(_,neutral).
+posVP--> pronoun, adv(_,positive), v(_,positive).
+posVP--> pronoun, adv(_,neutral), v(_,positive).
+posVP--> pronoun, adv(_,positive), v(_,neutral).
+
+
+neuVP--> v(_,neutral).
+neuVP--> pronoun, v(_,neutral).
+neuVP--> adj(_,neutral), v(_,neutral).
+neuVP--> v(_,neutral), adv(_,neutral).
+neuVP--> adv(neutral), v(neutral).
+neuVP--> pronoun, adv(neutral), v(neutral).
+
+negVP-->  v(_,negative).
+negVP--> pronoun, v(_,negative).
+negVP--> adj(_,negative), v(_,neutral).
+negVP--> adj(_,negative), v(_,negative).
+negVP--> v(_,negative), adj(_,neutral).
+negVP--> v(_,negative), adj(_,negative).
+negVP--> adv(_,negative), v(_,negative).
+negVP--> adv(_,neutral), v(_,negative).
+negVP--> adv(_,negative), v(_,neutral).
+negVP--> pronoun, adv(_.negative), v(_,negative).
+negVP--> pronoun, adv(_,neutral), v(_,negative).
+negVP--> pronoun, adv(_,negative), v(_,neutral).
+negVP--> adj(_,negative), v(_,positive).
+negVP--> v(_,negative), adj(_,positive).
+negVP--> adv(_,positive), v(_,negative).
+negVP--> adv(_,negative), v(_,positive).
+negVP--> pronoun, adv(_,positive), v(_,negative).
+negVP--> pronoun, adv(_,negative), v(_,positive).
+
