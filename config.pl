ac52fbb93bec8bae4973f632a4be34e9bcf9712b
JoshL <joshua.licudo.753@my.csun.edu>
2021-05-09 09:11:24 -0700

    Pull in + format Bezan's cleaned tweets; remove data/ from .gitignore

--- config.pl
+++ config.pl
@@ -2,6 +2,7 @@
 
 config(sarcastic_data_file_name, "data/sarcasticTweets.pl") :- !.
 config(normal_data_file_name, "data/normalTweets.pl") :- !.
+config(clean_data_file_name, "data/cleanTweets.pl") :- !.
 config(sarcasm_score_lower_bound, 0.175) :- !.
 config(sarcasm_score_upper_bound, 0.225) :- !.
 config(results_file_name, "results.csv") :- !.
