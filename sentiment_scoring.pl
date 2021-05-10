e9e243d9f396e19c15a9ebbd164227abba7c33d4
JamisonBall <jaball1993@aol.com>
2021-05-10 08:04:43 -0700

    Add Ratio Scoring Heuristic

--- sentiment_scoring.pl
+++ sentiment_scoring.pl
@@ -20,3 +20,35 @@ score_([Word|Rest], PosAcc, PosTotal, NegAcc, NegTotal) :-
 score(X, PosTotal, NegTotal) :-
     score_(X, 0, PosTotal, 0, NegTotal).
 
+cnt_np([], PosCount, PosCount, NegCount, NegCount).
+cnt_np([Word|Rest], PosAcc, PosCount, NegAcc, NegCount):-
+    lookup(Word, PosScore, NegScore),
+    PosScore > NegScore,
+    NewPosAcc is PosAcc + 1,
+    cnt_np(Rest, NewPosAcc, PosCount, NegAcc, NegCount).
+cnt_np([Word|Rest], PosAcc, PosCount, NegAcc, NegCount):-
+    lookup(Word, PosScore, NegScore),
+    PosScore < NegScore,
+    NewNegAcc is NegAcc + 1,
+    cnt_np(Rest, PosAcc, PosCount, NewNegAcc, NegCount).
+cnt_np([Word|Rest], PosAcc, PosCount, NegAcc, NegCount):-
+    lookup(Word, PosScore, NegScore),
+    PosScore =:= NegScore,
+    not(PosScore =:= 0),
+    NewPosAcc is PosAcc + 1,
+    NewNegAcc is NegAcc + 1,
+    cnt_np(Rest, NewPosAcc, PosCount, NewNegAcc, NegCount).
+cnt_np([Word|Rest], PosAcc, PosCount, NegAcc, NegCount):-
+    lookup(Word, PosScore, NegScore),
+    PosScore =:= NegScore,
+    PosScore =:= 0,
+    cnt_np(Rest, PosAcc, PosCount, NegAcc, NegCount).
+
+count_pos_neg(X, PosCount, NegCount):-
+    cnt_np(X, 0, PosCount, 0, NegCount).
+
+pos_neg_ratio(Sentence, PosRatio, NegRatio):-
+    length(Sentence, Len),
+    count_pos_neg(Sentence, PosScore, NegScore),
+    PosRatio is PosScore / Len,
+    NegRatio is NegScore / Len.
