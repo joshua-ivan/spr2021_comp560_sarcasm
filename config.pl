5440c60da41a34cc02f82a8605015f989823c37f
JoshL <joshua.licudo.753@my.csun.edu>
2021-05-11 20:27:44 -0700

    Add cleanNormalTweets.pl

--- config.pl
+++ config.pl
@@ -2,7 +2,8 @@
 
 config(sarcastic_data_file_name, "data/sarcasticTweets.pl") :- !.
 config(normal_data_file_name, "data/normalTweets.pl") :- !.
-config(clean_data_file_name, "data/cleanTweets.pl") :- !.
+config(clean_sarcastic_data_file_name, "data/cleanSarcasticTweets.pl") :- !.
+config(clean_normal_data_file_name, "data/cleanNormalTweets.pl") :- !.
 config(sarcasm_score_lower_bound, 0.175) :- !.
 config(sarcasm_score_upper_bound, 0.225) :- !.
 config(results_file_name, "results.csv") :- !.
